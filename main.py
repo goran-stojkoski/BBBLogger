7639d1f51fa2927b3b8508c53c26afd24b83bec8
Goran Stojkoski <s3017862@student.rmit.edu.au>
2021-02-21 21:39:59 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+file_location = r"C:\Users\S3017862\OneDrive\Documents\BBB\E-brewR2.4_ADDIS PALE_BREW41-42_211070_01.xlsm"
+brewlog_df = pd.read_excel(file_location, sheet_name="BREWLOG",engine='openpyxl')
+
+#%%
+
+cell_locations = {
+    'brew_name': (1,1),
+    'brew_type': (2,1),
+    'recipe_ver': (2,4),
+    'date': (4,4),
+    'length': (3,1),
+    'brew_no': (4,1),
+    'temp_amb_c': (5,1),
+    'vol': (7,1),
+    'digi_temp_c': (7,3),
+    'mash_start_time': (9,2),
+    'mash_all_in_time': (9,6),
+    'mash_2min_temp': (12,4),
+    'mash_2min_ph': (12,5),
+    'mash_2min_sg': (12,6),
+    'mash_2min_time': (12,7),
+    'mash_15min_temp': (15, 4),
+    'mash_15min_ph': (15, 5),
+    'mash_15min_sg': (15, 6),
+    'mash_15min_time': (15, 7),
+    'mash_60min_temp': (18, 4),
+    'mash_60min_ph': (18, 5),
+    'mash_60min_sg': (18, 6),
+    'mash_60min_time': (18, 7),
+    'fist_runnings_temp': (31,4),
+    'fist_runnings_ph': (31, 5),
+    'fist_runnings_sg': (31, 6),
+    'fist_runnings_time': (31, 7),
+    'fist_runnings_cloudy': (32, 6),
+    'fist_runnings_clear': (33, 6),
+    'fist_runnings_brilliant': (34, 6),
+    'final_runnings_temp': (36, 4),
+    'final_runnings_ph': (36, 5),
+    'final_runnings_sg': (36, 6),
+    'final_runnings_time': (36, 7),
+    'final_runnings_cloudy': (37, 6),
+    'final_runnings_clear': (38, 6),
+    'final_runnings_brilliant': (39, 6),
+    'runnings_time': (41,7),
+    'pre_boil_vol': (43,1),
+    'post_boil_vol': (45,1),
+    'post_boil_ph': (45,5),
+    'post_boil_sg': (45,6),
+    'vol_into_fermenter': (51,1),
+    'liquir_back_vol': (53,1),
+    'fv_number': (51,5),
+    'fermenter_total_vol': (55,1),
+    'fermenter_ph': (55, 5),
+    'fermenter_sg': (55, 6),
+    'fermenter_o2ppm': (55, 7)
+}
+
+for key,val in cell_locations.items():
+    print(key,brewlog_df.iloc[val])
